3b86641fafa9d32c4d4fd34baa749963e6d223fc
XHFkindergarten <XHFkindergarten@XHFkindtendeMBP.lan>
2019-05-02 21:48:36 +0800

    添加了远端服务器配置

--- config/index.js
+++ config/index.js
@@ -12,7 +12,6 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       // '/api': {  //使用"/api"来代替"http://f.apiplus.c" 
-      //     //target: 'http://192.168.0.102:8080', //源地址 
       //     target: 'http://127.0.0.1:3000', //源地址 
       //     changeOrigin: true, //改变源 
       //     pathRewrite: { 
